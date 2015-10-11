6156a26aa3d110ea92de9aac9a903e1d8080c84a
Sagar Kudale <sagar.kudale@cuelogic.co.in>
2015-10-11 23:10:16 +0530

    Added Alamofire framework

--- GalleryApp/GalleryApp/ViewController.swift
+++ GalleryApp/GalleryApp/ViewController.swift
@@ -7,12 +7,26 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        Alamofire.request(.GET, "http://httpbin.org/get", parameters: ["foo": "bar"])
+            .responseJSON { response in
+                print(response.request)  // original URL request
+                print(response.response) // URL response
+                print(response.data)     // server data
+                print(response.result)   // result of response serialization
+                
+                if let JSON = response.result.value {
+                    print("JSON: \(JSON)")
+                }
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
