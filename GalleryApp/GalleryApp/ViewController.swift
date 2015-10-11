37e569c430c3197dd5256870e9bc60959b978662
Sagar Kudale <sagar.kudale@cuelogic.co.in>
2015-10-11 13:09:51 +0530

    Initial commit

---
+++ GalleryApp/GalleryApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GalleryApp
+//
+//  Created by Sagar Kudale on 10/11/15.
+//  Copyright © 2015 Sagar Kudale. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
